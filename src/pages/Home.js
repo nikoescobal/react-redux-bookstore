8ffaaaaf3e6174d89c41fd473f095af89844f9f7
Niko Escobal <niko.escobal@gmail.com>
2021-11-14 00:38:35 +0800

    Add clear function to form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,66 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import BookList from '../components/BookList';
-import { addBook } from '../redux/reducers/bookReducer/bookActions';
+/* stylelint-disable */
+/* eslint-disable */
+
+import React from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import BookList from "../components/BookList";
+import { addBook } from "../redux/reducers/bookReducer/bookActions";
 
 function Home() {
   const dispatch = useDispatch();
-  const [value, setValue] = React.useState('');
+  const [book, setBook] = React.useState({
+    genre: "",
+    title: "",
+    author: "",
+    completed: "",
+    chapter: "",
+  });
+  // const [title, setTitle] = React.useState('');
+  // const [author, setAuthor] = React.useState('');
+  // const [completed, setCompleted] = React.useState('');
+  // const [chapter, setChapter] = React.useState('');
+
+  const updateObj = (e) => {
+    e.preventDefault(),
+      setBook({
+        ...book,
+        [e.target.name]: e.target.value,
+      });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(
       addBook({
         id: uuidv4(),
-        genre: 'Romance',
-        title: value,
-        author: 'Michael Garyssan',
-        completed: 64,
-        chapter: 'Chapter 17',
-      }),
+        genre: book.genre,
+        title: book.title,
+        author: book.author,
+        completed: book.completed,
+        chapter: book.chapters,
+      })
     );
+    setBook({
+      genre: "",
+      title: "",
+      author: "",
+      completed: "",
+      chapter: "",
+    });
   };
 
   return (
     <div>
       <BookList />
-      <form
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <input onChange={(e) => setValue(e.target.value)} type="text" />
-        <button type="submit">Add</button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input value={book.genre} name='genre' onChange={updateObj} />
+        <input value={book.title} name='title' onChange={updateObj} />
+        <input value={book.author} name='author' onChange={updateObj} />
+        <input value={book.completed} name='completed' onChange={updateObj} />
+        <input value={book.chapter} name='chapter' onChange={updateObj} />
+
+        <button type='submit'>Add</button>
       </form>
     </div>
   );
